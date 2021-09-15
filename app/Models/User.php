2f81de0ad3ae900aa62f8c04431d6f43b71b26ef
albertbiszta <albertbiszta@gmail.com>
2021-09-15 19:19:22 +0200

    User model fixes, method for most active users

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,4 +8,48 @@ use Illuminate\Database\Eloquent\Model;
 class User extends Model
 {
     use HasFactory;
+
+    protected $casts = [
+        'address' => 'array',
+        'company' => 'array',
+    ];
+    protected $guarded = [];
+    protected $primaryKey = 'id';
+    public $incrementing = false;
+    public $timestamps = true;
+
+    /**
+     *  User and Post relation: User can add many posts
+     */
+    public function posts()
+    {
+        return $this->hasMany(Post::class, 'userId');
+    }
+
+
+    /**
+     * Checks users with the most posts in the last week. [DESC]
+     * [User ID => number of posts]
+     * @param int $userLimit
+     * @return array
+     */
+    public static function mostActiveUsers(int $numberOfUsers): array
+    {
+        $numberOfPosts = [];
+        $date = Carbon::today()->subDays(7);
+        $users = User::all();
+
+        foreach ($users as $user) {
+            $numberOfPosts[$user->id] = DB::table('posts')
+                ->where('userId', '=', $user->id)
+                ->where('updated_at', '>=', $date)
+                ->count();
+        }
+
+        // Top users: [id => number of posts]
+        $topUsersCollection = collect($numberOfPosts)->sortDesc()->take($numberOfUsers);
+        $topUsers = $topUsersCollection->toArray();
+
+        return $topUsers;
+    }
 }
