d2db8c24de635db30e0eb7c29e1718609979f962
albertbiszta <albertbiszta@gmail.com>
2021-09-15 19:21:28 +0200

    Post Model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
+    protected $primaryKey = 'id';
+    public $incrementing = false;
+    public $timestamps = true;
+
+    // Post belongs to one author
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'userId');
+    }
 }
