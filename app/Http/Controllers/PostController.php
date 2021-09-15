a3c8ddea16f1d3f7e9ba92b4da169ceab5cfe4fa
albertbiszta <albertbiszta@gmail.com>
2021-09-15 19:36:17 +0200

    PostController: basic methods: index, add or update user

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
+class PostController extends Controller
+{
+    // URL to external API
+    private $postApiUrl = 'https://jsonplaceholder.typicode.com/posts';
+
+    public function index()
+    {
+        $posts = Post::all();
+
+        return view('posts.index', [
+            'posts' => $posts,
+        ]);
+
+    }
+
+    /**
+     * Decides whether to update or add
+     */
+    public function addOrUpdate()
+    {
+        $apiPosts = Http::get($this->postApiUrl)->json();
+        foreach ($apiPosts as $apiPost) {
+            $post = Post::find($apiPost['id']);
+            $post ? $post->update($apiPost) : $this->addPost($apiPost);
+        }
+
+    }
+
+    /**
+     * Saves a new post
+     */
+    public function addPost($apiPost)
+    {
+        $post = new Post($apiPost);
+        $post->save();
+    }
+
+
+    /**
+     * Shows the post details page
+     * @param Post $post
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
+     */
+    public function show(Post $post)
+    {
+        return view('posts.show', [
+            'post' => $post,
+        ]);
+
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -25,3 +25,10 @@ Route::get('/users/add-or-update', [UserController::class, 'addOrUpdate']);
 //Route::get('/users/most-active', [UserController::class, 'mostActive'])->name('users.mostActive');
 //Route::get('/users/{user}', [UserController::class, 'show']);
 
+
+// Post routes
+Route::get('/posts', [PostController::class, 'index']);
+Route::get('/posts/add-or-update', [PostController::class, 'addOrUpdate']);
+Route::get('/posts/{post}', [PostController::class, 'show']);
+
+
